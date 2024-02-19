36b32b4df46ada6d27f1288a79315b751af2676a
Johan van Tent <tentvanjohan@hotmail.com>
2024-02-19 08:57:27 +0100

    code voor lokaal runnen buiten Rstudio

---
+++ chemie_diagnostics_shiny.bat
@@ -0,0 +1,1 @@
+"C:/Program Files/R/R-4.3.0/bin/x64/Rscript.exe" -e "source('C:/R/chemie_diagnostics_shiny/run.R')"
---
+++ run.R
@@ -0,0 +1,4 @@
+library(shiny)
+setwd("C:/R/chemie_diagnostics_shiny")
+# quarto::quarto_run("chemie_actueel.qmd")
+runApp(launch.browser = TRUE)
